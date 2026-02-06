99c7ee6845366edd0bf41b3a535e30dd5cd14666
Akanksha <ak.tech106@gmail.com>
2026-02-07 01:40:55 +0530

    Refactor Terraform configuration for EC2 instance

--- terraform-files/main.tf
+++ terraform-files/main.tf
@@ -1,24 +1,34 @@
 resource "aws_instance" "test-server" {
-  ami = "ami-0532be01f26a3de55"
-  instance_type = "t3.small"
-  key_name = "zomato-keypair"
-  vpc_security_group_ids = ["sg-01da398717a940515"]
+  ami                         = "ami-0532be01f26a3de55"
+  instance_type               = "t3.small"
+  key_name                    = "zomato-keypair"
+  vpc_security_group_ids      = ["sg-01da398717a940515"]
+
   connection {
-     type = "ssh"
-     user = "ec2-user"
-     private_key = file("./zomato-keypair.pem")
-     host = self.public_ip
-     }
+    type        = "ssh"
+    user        = "ec2-user"
+    private_key = file("./zomato-keypair.pem")
+    host        = self.public_ip
+  }
+
+  # Wait for EC2 to be ready
   provisioner "remote-exec" {
-     inline = ["echo 'wait to start the instance' "]
+    inline = [
+      "echo 'Waiting for EC2 instance to be ready...'"
+    ]
   }
+
   tags = {
-     Name = "test-server"
-     }
-  provisioner "local-exec" {
-     command = "echo ${aws_instance.test-server.public_ip} > inventory"
-     }
-  provisioner "local-exec" {
-     command = "ansible-playbook /var/lib/jenkins/workspace/zomatoapp/terraform-files/ansiblebook.yml"
-     }
+    Name = "test-server"
   }
+
+  # Create inventory file for Ansible
+  provisioner "local-exec" {
+    command = "echo '${self.public_ip}' > ./inventory"
+  }
+
+  # Run Ansible playbook
+  provisioner "local-exec" {
+    command = "ANSIBLE_CONFIG=./ansible.cfg ansible-playbook ./ansiblebook.yml"
+  }
+}
